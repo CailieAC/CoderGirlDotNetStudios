3ea31fdc7b8609f542568cd4da3f893890618d7b
Cailie Carlile <cailiecarlile@gmail.com>
2019-03-06 18:46:20 -0600

    scrabble

---
+++ LaunchScrabble/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace LaunchScrabble
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, 2, 2, 3, 6, 33 };
+            string output = String.Join(',', numbers);
+            Console.WriteLine(output);
+            Console.ReadLine();
+        }
+    }
+}
